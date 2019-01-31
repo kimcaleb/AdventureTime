582d1f5caba25360b424c0111252da79f86fa530
Sean Berrie <seanberrie21@gmail.com>
2019-01-31 13:05:42 -0800

    added logout function

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { Switch, Route } from 'react-router-dom'
 import Login from './components/Login'
 import httpClient from './utilities/httpClient'
 import Signup from './components/Signup'
+import Logout from './components/Logout'
 
 class App extends Component {
 
@@ -17,16 +18,22 @@ class App extends Component {
     this.setState({currentUser:httpClient.getCurrentUser()})
   }
 
-
+  logOut = () => {
+    httpClient.logout()
+    this.setState({ currentUser: null })
+  }
 
 
   render() {
     return (
       <Layout currentUser={this.state.currentUser}>
         <Switch>
-        <Route exact path="/" render={(props) => {
+          <Route exact path="/" render={(props) => {
 						return <Login {...props} onLoginSuccess={this.onAuthSuccess} />
 					}}/>
+          <Route exact path='/logout' render={() => {
+            return <Logout logOut={this.logOut} /> 
+          }} />
           <Route path='/profile' component={Home} />
           <Route path='/signup' render={(props) => {
 						return <Signup {...props} onSignupSuccess={this.onAuthSuccess} />
