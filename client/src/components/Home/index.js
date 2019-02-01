bc72b5b16bf7989f438188a131761adbb169c892
Sean Berrie <seanberrie21@gmail.com>
2019-02-01 11:54:18 -0800

    pulling data from model of city into view at home

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom'
 export default class Profile extends Component {
   render () {
     let { cities } = this.props.currentUser
+    debugger
     return (
       <div>
         <h1>Adventure Time</h1>
         <Link to='/profile/city'>City</Link>
-        <p>{this.props.currentUser.cities.city}</p>
+        <p>{cities[0].city}</p>
 
       </div>
 
