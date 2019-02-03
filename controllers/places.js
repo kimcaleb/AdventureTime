fc25942b29677e030d7ef4941054d57822dfa35a
Caleb Kim <calebkim2005@yahoo.com>
2019-02-02 16:30:53 -0800

    Fixed Delete Place Controller Method

--- controllers/places.js
+++ controllers/places.js
@@ -1,55 +1,53 @@
 const User = require('../models/User')
 
 module.exports = {
-  create: (req, res) => {
-    let { user_id, city_id } = req.params
-    User.findById(user_id, (err, user) => {
-      if (err) return res.json({ message: 'Error' })
-      let { cities } = user
-      let city = null
-      for (let i = 0; i < cities.length; i++) {
-        if (cities[i]._id == city_id) {
-          city = cities[i]
-        }
-      }
-      if (city === null) {
-        return console.log('No City Found')
-      }
-      city.places.push(req.body)
-      city.save(err => {
-        if (err) console.log('Could not update city')
-        user.save(err => {
-          if (err) console.log(err)
-          res.json({ message: 'Success', city })
-        })
-      })
-    })
-  },
+ create: (req, res) => {
+   let { user_id, city_id } = req.params
+   User.findById(user_id, (err, user) => {
+     if (err) return res.json({ message: 'Error' })
+     let { cities } = user
+     let city = null
+     for (let i = 0; i < cities.length; i++) {
+       if (cities[i]._id == city_id) {
+         city = cities[i]
+       }
+     }
+     if (city === null) {
+       return console.log('No City Found')
+     }
+     city.places.push(req.body)
+     city.save(err => {
+       if (err) console.log('Could not update city')
+       user.save(err => {
+         if (err) console.log(err)
+         res.json({ message: 'Success', city })
+       })
+     })
+   })
+ },
 
-  destroy: (req, res) => {
-    let { user_id, city_id, place_id } = req.params
-    User.findById(user_id, (err, user) => {
-      if (err) return res.json({ message: 'error' })
-      let { cities } = user
-      let city = null
-      for (let i = 0; i < cities.length; i++) {
-        if (cities[i]._id == city_id) {
-          city = cities[i]
-        }
-      }
-      if (city === null) {
-        return console.log('no city found')
-      }
-      city.places = city.places.filter(item => {
-        return item._id !== place_id
-      })
-      city.save(err => {
-        if (err) console.log('could not delete place')
-        user.save(err => {
-          if (err) console.log('could not update user')
-          res.json({ message: 'success', user })
-        })
-      })
-    })
-  }
+ destroy: (req, res) => {
+   let { user_id, city_id, place_id } = req.params
+   User.findById(user_id, (err, user) => {
+     if (err) return res.json({ message: 'error' })
+     let { cities } = user
+     let city = null
+     for (let i = 0; i < cities.length; i++) {
+       if (cities[i]._id == city_id) {
+         city = cities[i]
+       }
+     }
+     if (city === null) {
+       return console.log('no city found')
+     }
+     city.places.splice(city.places.indexOf(place_id),1)
+     city.save(err => {
+       if (err) console.log('could not delete place')
+       user.save(err => {
+         if (err) console.log('could not update user')
+         res.json({ message: 'success', user })
+       })
+     })
+   })
+ }
 }
