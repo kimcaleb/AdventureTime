fe584650a448508ace199d276f27df429d94f832
Caleb Kim <calebkim2005@yahoo.com>
2019-02-05 15:54:51 -0800

    Added more styling

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -27,18 +27,17 @@ export default class Login extends Component {
         return (
             <div className="hero">
                 <div className="login">
-                <svg viewBox="0 0 500 500">
-                        <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />                   
-                        <text width="500">
-                            <textPath href="#curve">
-                                Adventure Time
-                            </textPath>
-                        </text>
-                </svg>  
-           
+                    <svg viewBox="0 0 500 500">
+                            <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />                   
+                            <text width="500">
+                                <textPath href="#curve">
+                                    Adventure Time
+                                </textPath>
+                            </text>
+                    </svg>  
                 <h3>Login</h3>
                     <div className="form">
-                        <form onSubmit={this.handleSubmit} > 
+                        <form onSubmit={this.handleSubmit}> 
                             <label className="label">Email</label>
                             <input
                             className='input'
@@ -57,10 +56,10 @@ export default class Login extends Component {
                                 onChange={this.handleChange}
                                 value={password}
                                 />
-                            <input className="btn" type="submit"/>
+                            <input className="btn" type="submit" id="submitBtnForEditingProfile"/>
                         </form>
-                    </div>
-            </div>
+                     </div>
+                 </div>
         </div>          
         )
 
